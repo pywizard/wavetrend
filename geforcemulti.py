1893ecc48b0686f52e2ea15ba257575fed97a37f
nr <planets.riddler@gmail.com>
2019-08-08 04:14:02 +0700

    generate flat market recognition values each 10 seconds.

--- geforcemulti.py
+++ geforcemulti.py
@@ -929,7 +929,7 @@ class ChartRunner(QtCore.QThread):
             else:
               indicator.generate_values(popen, phigh, plow, pclose, pvol)
 
-              if time.time() - indicator_update_time > 10 or current_candle_type != candle_type or current_trade_type != trade_type or current_bband_type != bband_type:
+              if time.time() - indicator_update_time > 10:
                   if window_configs[self.tab_index].ai_enabled == True:
                       stddev = talib.STDDEV(np.array(pclose), timeperiod=20, nbdev=1)
                       stddev_on_axis = []
@@ -940,6 +940,8 @@ class ChartRunner(QtCore.QThread):
                           self.parent.neural_network.ai_trending_market = True
                       else:
                           self.parent.neural_network.ai_trending_market = False
+
+              if time.time() - indicator_update_time > 10 or current_candle_type != candle_type or current_trade_type != trade_type or current_bband_type != bband_type:
                   indicator.update()
             
             xaxis_start = indicator.xaxis_get_start()
